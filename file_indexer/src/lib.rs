626225a338e1f010d058c1ff744034df790f2124
dsgallups <dsgallups@protonmail.com>
2025-11-21 07:23:06 -0500

    scaffold channels for file indexer

--- file_indexer/src/lib.rs
+++ file_indexer/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::PathBuf;
+
+use tokio::sync::{
+    mpsc::{UnboundedReceiver, UnboundedSender},
+    oneshot,
+};
+
+pub enum IndexRequest {
+    Close(oneshot::Sender<Vec<IndexEvent>>),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub enum IndexEvent {
+    Foo(i32),
+    Finished,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct FileIndexer {
+    request_chan: UnboundedReceiver<IndexRequest>,
+    path: PathBuf,
+}
+
+impl FileIndexer {
+    pub fn new(path: impl Into<PathBuf>, request: UnboundedReceiver<IndexRequest>) -> Self {
+        Self {
+            path: path.into(),
+            request_chan: request,
+        }
     }
+
+    pub async fn run(self, channel: UnboundedSender<IndexEvent>) {}
 }

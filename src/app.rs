a9d389d0c47fea2b2365d586f85f30e76bdccda5
dsgallups <dsgallups@protonmail.com>
2025-11-21 05:17:29 -0500

    remove auth

--- src/app.rs
+++ src/app.rs
@@ -47,7 +47,6 @@ impl Hooks for App {
 
     fn routes(_ctx: &AppContext) -> AppRoutes {
         AppRoutes::with_default_routes() // controller routes below
-            .add_route(controllers::auth::routes())
     }
     async fn connect_workers(ctx: &AppContext, queue: &Queue) -> Result<()> {
         queue.register(DownloadWorker::build(ctx)).await?;

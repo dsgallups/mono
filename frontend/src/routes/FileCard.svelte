ce1d5f6336b658878c5388479756dff2dd962406
dsgallups <dsgallups@protonmail.com>
2025-11-24 10:34:05 -0500

    vibe some icon placeholders

--- frontend/src/routes/FileCard.svelte
+++ frontend/src/routes/FileCard.svelte
@@ -54,7 +54,54 @@
 </script>
 
 <div class="flex flex-1 shrink flex-col items-center border border-stone-400 p-4">
-	<div class="h-10 w-10 bg-red-400"></div>
+	<div class="flex h-10 w-10 items-center justify-center">
+		{#if file.file_type === 'text'}
+			<svg
+				class="h-16 w-16 text-gray-600"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+				></path>
+			</svg>
+		{:else if file.file_type === 'jpeg'}
+			<svg
+				class="h-16 w-16 text-blue-600"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+				></path>
+			</svg>
+		{:else}
+			<svg
+				class="h-16 w-16 text-gray-400"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+				></path>
+			</svg>
+		{/if}
+	</div>
 	<p>{file.title}</p>
 	{#if hasChunks}
 		<div class="min-w-100">
